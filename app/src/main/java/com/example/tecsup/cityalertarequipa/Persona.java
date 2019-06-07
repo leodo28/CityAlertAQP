f95d3c14db6b2b5096fed219f82d845a4df53179
Leo <leodo18@gmail.com>
2019-06-07 18:55:47 -0500

    Clase persona

---
+++ app/src/main/java/com/example/tecsup/cityalertarequipa/Persona.java
@@ -0,0 +1,70 @@
+package com.example.tecsup.cityalertarequipa;
+
+public class Persona {
+    String nombre;
+    String apellido;
+    String dni;
+    String correo;
+    String telefono;
+    String direccion;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String dni, String correo, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
